9a16132f11c50bd160d04f8d2048b8e2745132ff
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2025-03-09 16:42:13 -0300

    feat: Drawing a word and displaying the letter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import styles from "./app.module.css"
+import { useEffect, useState } from "react"
+
+import { WORDS } from "./utils/words"
 
 import { Input } from "./components/Input"
 import { Button } from "./components/Button"
@@ -8,24 +11,44 @@ import { Header } from "./components/Header"
 import { LettersUsed } from "./components/LettersUsed"
 
 export default function App() {
+    const [ letter, setLetter ] = useState("")
+    const [ attempts, setAttempts ] = useState(0)
+    const [ challenge, setChallenge ] = useState<Challenge | null>(null)
 
     function handleRestartGame() {
         alert("Reiniciar o jogo!")
     }
 
+    function startGame() {
+        const index = Math.floor(Math.random() * WORDS.length)
+        const randomWord = WORDS[index]
+
+        setChallenge(randomWord)
+        setAttempts(0)
+        setLetter("")
+    }
+
+    useEffect(() => {
+        startGame()
+    }, [])
+
+    if(!challenge) {
+        return
+    }
+
     return (
         <div className={styles.container}>
             <main>
-                <Header current={5} max={10} onRestart={handleRestartGame} />
+                <Header current={attempts} max={10} onRestart={handleRestartGame} />
 
                 <Tip tip="Uma das linguagem de programação dinâmica" />
 
                 <div className={styles.word}>
-                    <Letter value="R"/>
-                    <Letter value="E"/>
-                    <Letter value="A"/>
-                    <Letter value="C"/>
-                    <Letter value="T"/>
+                    {
+                        challenge.word.split("").map(() => (
+                            <Letter value="" />
+                        ))
+                    }
                 </div>
 
                 <h4>Palpite</h4>
